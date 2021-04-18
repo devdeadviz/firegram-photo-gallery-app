f7a635d0825275ee6e9e607350b6ec6830558a4b
devdeadviz <kuldeepgupta485@gmail.com>
2021-04-18 21:03:44 +0530

    img problem fixed

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -3,17 +3,16 @@ import useFirestore from "../hooks/useFirestore";
 
 const ImageGrid = () => {
   const { docs } = useFirestore("images");
-  console.log(docs);
 
   return (
     <div className="img-grid">
       {docs &&
-        // eslint-disable-next-line array-callback-return
         docs.map((doc) => {
-          console.log(doc.url, doc.id);
-          <div className="img-wrap" key={doc.id}>
-            <img src={doc.url} alt="uploaded pic"></img>
-          </div>;
+          return (
+            <div className="img-wrap" key={doc.id}>
+              <img src={doc.url} alt="uploaded pic"></img>
+            </div>
+          );
         })}
     </div>
   );
