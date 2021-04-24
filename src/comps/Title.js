8464c12c74fedfb58d2b411d008d0db3e1cbfee3
devdeadviz <kuldeepgupta485@gmail.com>
2021-04-24 19:51:26 +0530

    updating Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
       <h1>FireGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Upload Your Pictures here! 👇</p>
+      <p>Click to enlarge them!</p>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
